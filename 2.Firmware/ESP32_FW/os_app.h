f65c3f9da1fbda692134fe0dedad6522187ed153
BadFatCat0919 <543015378@qq.com>
2021-01-23 23:11:15 +0800

    创建文件

---
+++ 2.Firmware/ESP32_FW/os_app.h
@@ -0,0 +1,11 @@
+/**
+ * @file os_app.h
+ * @author BadFatCat0919 (543015378@qq.com)
+ * @brief 操作系统各应用程序的头文件
+ * @date 2021-01-23
+ * 
+ * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * 
+ */
---
+++ 2.Firmware/ESP32_FW/os_includes.h
@@ -0,0 +1,11 @@
+/**
+ * @file os_includes.h
+ * @author BadFatCat0919 (543015378@qq.com)
+ * @brief 操作系统相关代码的头文件
+ * @date 2021-01-23
+ * 
+ * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * 
+ */
