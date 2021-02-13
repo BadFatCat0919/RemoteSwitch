295a07418b53e1f4e50d353317cb166072b8630b
BadFatCat0919 <543015378@qq.com>
2021-02-13 18:58:44 +0800

    加入电容触摸驱动

---
+++ 2.Firmware/ESP32_FW/hw_touch.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file hw_touch.hpp
+ * @author BadFatCat0919 (543015378@qq.com)
+ * @brief 硬件层HW_Touch类
+ * @date 2021-01-24
+ */
+
+#ifndef __HW_TOUCH_HPP__
+#define __HW_TOUCH_HPP__
+
+#include <stdint.h>
+#include "hw_gpio.hpp"
+
+/**
+ * @brief 硬件层的HW_Touch类
+ */
+class HW_Touch
+{
+public:
+    /**
+     * @brief 创建一个新的HW_Touch对象
+     * @param pin 使用的gpio引脚
+     */
+    HW_Touch(uint8_t pin):
+        _gpio(pin, INPUT){}
+
+    /**
+     * @brief 销毁一个已创建的HW_Touch对象
+     */
+    ~HW_Touch() = default;
+
+    /**
+     * @brief 初始化
+     */
+    void init()
+    {
+        _gpio.init();
+    }
+
+    /**
+     * @brief 检测按键是否被按下
+     * @return true 被按下
+     * @return false 没被按下
+     */
+    bool scan()
+    {
+        return _gpio.status() == true;
+    }
+
+protected:
+    HW_GPIO _gpio;
+};
+
+#endif // __HW_TOUCH_HPP__
